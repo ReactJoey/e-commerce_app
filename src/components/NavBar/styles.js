746a9142b2859b486397c523d64b61a0e4e446d7
J. M. Alessi <j.m.alessi27@outlook.com>
2022-06-06 09:24:01 -0400

    navbar jsx + css

---
+++ src/components/NavBar/styles.js
@@ -0,0 +1,67 @@
+import { makeStyles, fade } from '@material-ui/core/styles';
+
+const drawerWidth = 0;
+
+export default makeStyles((theme) => ({
+  appBar: {
+    boxShadow: 'none',
+    borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+  },
+  title: {
+    flexGrow: 1.25,
+    alignItems: 'center',
+    display: 'flex',
+    textDecoration: 'none',
+  },
+  image: {
+    marginRight: '12px',
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  grow: {
+    flexGrow: 1.25,
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.1),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.2),
+    },
+    marginRight: theme.spacing(3),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 3),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(5)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '22ch',
+    },
+  },
+}));
